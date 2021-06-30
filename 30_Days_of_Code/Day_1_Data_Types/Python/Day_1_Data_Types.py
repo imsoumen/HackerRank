dc951bd5db34e4b4b3541f4fc54bdcf3090f2910
Soumen Saha <43337272+imsoumen@users.noreply.github.com>
2021-07-01 01:36:05 +0530

    Adding Day 1 Python Solution

---
+++ 30_Days_of_Code/Day_1_Data_Types/Python/Day_1_Data_Types.py
@@ -0,0 +1,19 @@
+
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+
+input_int = int(input())
+input_double = float(input())
+input_str = str(input())
+
+print(input_int + i)
+print(input_double + d)
+print(s + input_str)
